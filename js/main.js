b6901d59c993317adab4dc3401192ed1499329b8
Laura "Missha" Vara <152572519+vara-co@users.noreply.github.com>
2025-08-24 01:54:47 -0600

    scroll bar animation
    Added a JS file to incorporate animation to a scroll bar for "My Skills" section.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach(scroller => {
+        scroller.setAttribute('data-animated', true);
+    }) 
+}
