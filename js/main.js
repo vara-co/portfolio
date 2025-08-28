4f457522befbb144171df000c97d3fa327fe3b5b
Laura "Missha" Vara <152572519+vara-co@users.noreply.github.com>
2025-08-27 23:21:17 -0600

    updated file
    Added some changes to the scrolling bar for the skills

--- js/main.js
+++ js/main.js
@@ -7,7 +7,16 @@ if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 }
 
 function addAnimation() {
-    scrollers.forEach(scroller => {
-        scroller.setAttribute('data-animated', true);
-    }) 
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector(".scroller_inner");
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach((item) => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(duplicatedItem);
+        });
+    }); 
 }
